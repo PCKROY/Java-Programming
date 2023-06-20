2d7946c9ee828821ada98a9dac823281d1ea8fcb
Prokash Roy <Pro5080.r@gmail.com>
2023-06-20 12:37:42 +0600

    Random Number Game

---
+++ Loop/src/RandomNumGame.java
@@ -0,0 +1,17 @@
+import com.sun.source.tree.ContinueTree;
+
+public class RandomNumGame {
+    public static void main(String[] args) {
+        while ( true ) {
+            int num = (int) (Math.random() * 10 + 1);
+            if (num == 5)
+                break;
+
+         System.out.println(num + " ");
+
+
+
+        }
+
+    }
+}
