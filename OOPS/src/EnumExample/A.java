01c88236973c811479647a18fb9a4866c2b9c3b8
Prokash Roy <Pro5080.r@gmail.com>
2023-08-17 20:11:15 +0600

    EnumExample

---
+++ OOPS/src/EnumExample/A.java
@@ -0,0 +1,11 @@
+package EnumExample;
+
+public interface A {
+    void hello();
+}
+
+
+            /* Enums: enum is a special class that represents a group of constants (like final variables)
+            To create an enum, use the enum keyword (instead of class or interface), and separate the
+            constants with a comma. So we can define our own data type using enum class.
+            */
