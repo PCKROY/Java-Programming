7ab999235ea81ecb018327fbdc3b742463ea166e
Prokash Roy <Pro5080.r@gmail.com>
2023-08-23 20:30:46 +0600

    typecasting

---
+++ Kunal/src/Typecasting.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Typecasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        float num = input.nextFloat();
+//        System.out.println(num);
+//        int num1 = input.nextInt();
+//        System.out.println(num1);
+
+        // type casting
+//        int num = (int)(50.67f);
+//        System.out.println(num);
+
+        // Automatic type promotion in expression
+//        int a = 257;
+//        byte b = (byte)(a);     // 257 % 256 = 1
+//        System.out.println(b);
+
+//        byte a = 40;
+//        byte b = 50;
+//        byte c = 100;
+//        int d = (a * b) / c;        // here, for a * b java is automatically promotes byte to int.
+//        System.out.println(d);
+
+//        byte b = 50;
+//        b = (byte) (b * 2);     // have to type cast it.otherwise it will give error. cause for ' * ' it becomes int.
+
+//        int number  = 'A';     //  ESKII value of A will be printed.
+        System.out.println("হ্যালো");  // any languages can be printed. unicode features
+
+
+
+    }
+}
