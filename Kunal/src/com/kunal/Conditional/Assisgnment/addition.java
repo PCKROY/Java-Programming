bd6e11b981a4ee55679fd8ce95a8330289025d35
Prokash Roy <Pro5080.r@gmail.com>
2023-08-27 23:34:50 +0600

    addition

---
+++ Kunal/src/com/kunal/Conditional/Assisgnment/addition.java
@@ -0,0 +1,12 @@
+package com.kunal.Conditional.Assisgnment;
+
+public class addition {
+    // Addition of 2 numbers
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 100;
+
+        int sum  = a + b ;
+        System.out.println(sum);
+    }
+}
